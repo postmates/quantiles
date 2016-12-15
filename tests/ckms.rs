5d6ea5e0d60d2f4ea4ceb362ce2e95ddd99c80ee
Brian L. Troutwine <blt@postmates.com>
2016-12-15 12:03:40 -0800

    Resolve AFL discovered crashes
    This commit includes fixes to the initilization routine of CKMS.
    The changes to the end-user are documented but do not, I believe,
    constitute a change in API. Users are now protected from crashes
    at the extremes of 'error'.
    
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>

---
+++ tests/ckms.rs
@@ -0,0 +1,37 @@
+mod integration {
+    mod ckms {
+        extern crate quantiles;
+
+        use std::fs::File;
+        use std::io::Read;
+        use std::path::PathBuf;
+        use std::str::FromStr;
+        use self::quantiles::CKMS;
+
+        #[test]
+        fn test_run_afl_examples() {
+            let mut resource = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            resource.push("resources/afl_crashes_20161215.txt");
+
+            let mut f = File::open(resource).expect("could not open resource file");
+            let mut buffer = String::new();
+            f.read_to_string(&mut buffer).expect("could not read resource file");
+
+            for s in buffer.lines() {
+                let pyld: Vec<f64> = s.split_whitespace()
+                    .map(|f| f64::from_str(f))
+                    .filter(|f| f.is_ok())
+                    .map(|f| f.unwrap())
+                    .collect();
+
+                if pyld.len() >= 2 {
+                    let mut ckms = CKMS::new(pyld[0]);
+
+                    for f in &pyld[1..] {
+                        ckms.insert(*f)
+                    }
+                }
+            }
+        }
+    }
+}
