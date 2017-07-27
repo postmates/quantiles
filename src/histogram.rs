e876698076b68b7b9227233f91477dd9d0367add
Brian L. Troutwine <blt@postmates.com>
2017-07-27 16:55:04 -0700

    Add documentation to Histogram implementation
    This commit brings in documentation for the Histogram implementation.
    Need a little review on the text, I imagine, since there's lots of
    ways to make a Histogram and lots of histograms to cloud the mind.
    
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>

--- src/histogram.rs
+++ src/histogram.rs
@@ -3,9 +3,8 @@
 //! require bins to be equally sized. The user must specify upper bounds on bins
 //! via `Bounds`. The implementation includes a +Inf bound automatically.
 //!
-//! Storage cost is proportional to the number of bins.
-//!
-//! # Examples
+//! Storage cost is proportional to the number of bins. The implementation is
+//! biased in favor of writes.
 
 use std::cmp;
 use std::fmt;
@@ -17,7 +16,7 @@ pub enum Bound<T>
 where
     T: Copy,
 {
-    /// A finite upper bound. 
+    /// A finite upper bound.
     Finite(T),
     /// A positively infinite upper bound. We cheat when doing ordering and say
     /// that PosInf == PosInf. This is not strictly true but it's true enough
@@ -64,9 +63,7 @@ where
     }
 }
 
-/// The Histogram type.
-///
-/// TODO more docs to come 
+/// A binning histogram of unequal, pre-defined bins
 #[derive(Debug)]
 pub struct Histogram<T>
 where
@@ -116,7 +113,31 @@ impl<T> Histogram<T>
 where
     T: Copy + cmp::PartialOrd + fmt::Debug,
 {
-    /// TODO documentation
+    /// Create a new Histogram
+    ///
+    /// This Histogram is a binning histogram of unequal bins. The user is
+    /// responsible for defining the upper bounds of bins. Users are able to
+    /// query bin counts without exact bins but should be aware that the results
+    /// will only be approximate unless the explicit bin is used. See `total_*`
+    /// functions for details.
+    ///
+    /// # Examples
+    /// ```
+    /// use quantiles::histogram::{Bound, Histogram};
+    ///
+    /// let mut histo = Histogram::<u64>::new(vec![10, 256, 1987,
+    /// 1990]).unwrap();
+    /// for i in 0..2048 {
+    ///     histo.insert(i as u64);
+    /// }
+    ///
+    /// assert_eq!(histo.total_above(Bound::Finite(0)), 2048);
+    /// assert_eq!(histo.total_above(Bound::Finite(11)), 2037);
+    /// assert_eq!(histo.total_above(Bound::Finite(10)), 2037);
+    /// assert_eq!(histo.total_between(Bound::Finite(1987),
+    /// Bound::Finite(1990)), 3);
+    /// assert_eq!(histo.total_below(Bound::PosInf), 2048);
+    /// ```
     pub fn new(bounds: Vec<T>) -> Result<Histogram<T>, Error> {
         if bounds.is_empty() {
             return Err(Error::BoundsEmpty);
@@ -134,12 +155,28 @@ where
 
         Ok(Histogram {
             count: 0,
-            sum: None, // TODO make summations, tests for same 
+            sum: None, // TODO make summations, tests for same
             bins: bins,
         })
     }
 
-    /// TODO documentation
+    /// Insert a T into the Histogram
+    ///
+    /// Insertion will search for the appropriate bin and increase the counter
+    /// found there. If two bins `a` and `b` form a bin with `a < b` then `X`
+    /// will be placed into that bin if `a < X <= b`.
+    ///
+    /// # Examples
+    /// ```
+    /// use quantiles::histogram::{Bound, Histogram};
+    ///
+    /// let mut histo = Histogram::<u64>::new(vec![10, 100]).unwrap();
+    /// histo.insert(99 as u64);
+    /// histo.insert(100 as u64);
+    ///
+    /// assert_eq!(histo.total_between(Bound::Finite(10), Bound::Finite(100)),
+    /// 2);
+    /// ```
     pub fn insert(&mut self, value: T) -> () {
         let mut idx = 0;
         let val_bound = Bound::Finite(value);
@@ -155,16 +192,38 @@ where
         self.count += 1;
     }
 
-    /// Returns the total number of items stored in the histogram 
-    /// 
-    /// TODO documentation
+    /// Returns the total number of items 'stored' in the histogram
+    ///
+    /// # Examples
+    /// ```
+    /// use quantiles::histogram::{Bound, Histogram};
+    ///
+    /// let mut histo = Histogram::<u64>::new(vec![10, 256, 1987,
+    /// 1990]).unwrap();
+    /// for i in 0..2048 {
+    ///     histo.insert(i as u64);
+    /// }
+    ///
+    /// assert_eq!(histo.count(), 2048);
+    /// ```
     pub fn count(&self) -> usize {
         self.count
     }
 
     /// Total number of items below supplied upper_bound
     ///
-    /// TODO documentation
+    /// # Examples
+    /// ```
+    /// use quantiles::histogram::{Bound, Histogram};
+    ///
+    /// let mut histo = Histogram::<u64>::new(vec![10, 256, 1987,
+    /// 1990]).unwrap();
+    /// for i in 0..2048 {
+    ///     histo.insert(i as u64);
+    /// }
+    ///
+    /// assert_eq!(histo.total_below(Bound::PosInf), 2048);
+    /// ```
     pub fn total_below(&self, upper: Bound<T>) -> usize {
         let mut count = 0;
         for &(ref bound, cnt) in &self.bins {
@@ -179,7 +238,20 @@ where
 
     /// Total number of items above supplied lower_bound
     ///
-    /// TODO documentation
+    /// # Examples
+    /// ```
+    /// use quantiles::histogram::{Bound, Histogram};
+    ///
+    /// let mut histo = Histogram::<u64>::new(vec![10, 256, 1987,
+    /// 1990]).unwrap();
+    /// for i in 0..2048 {
+    ///     histo.insert(i as u64);
+    /// }
+    ///
+    /// assert_eq!(histo.total_above(Bound::Finite(0)), 2048);
+    /// assert_eq!(histo.total_above(Bound::Finite(11)), 2037);
+    /// assert_eq!(histo.total_above(Bound::Finite(10)), 2037);
+    /// ```
     pub fn total_above(&self, lower: Bound<T>) -> usize {
         let mut count = 0;
         for &(ref bound, cnt) in &self.bins {
@@ -193,7 +265,19 @@ where
 
     /// Total number of items between [lower_bound, upper_bound)
     ///
-    /// TODO documentation
+    /// # Examples
+    /// ```
+    /// use quantiles::histogram::{Bound, Histogram};
+    ///
+    /// let mut histo = Histogram::<u64>::new(vec![10, 256, 1987,
+    /// 1990]).unwrap();
+    /// for i in 0..2048 {
+    ///     histo.insert(i as u64);
+    /// }
+    ///
+    /// assert_eq!(histo.total_between(Bound::Finite(1987),
+    /// Bound::Finite(1990)), 3);
+    /// ```
     pub fn total_between(&self, lower: Bound<T>, upper: Bound<T>) -> usize {
         if lower >= upper {
             return 0;
